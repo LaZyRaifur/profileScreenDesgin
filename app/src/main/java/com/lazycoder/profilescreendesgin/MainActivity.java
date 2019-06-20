2986aa975dc3904f20198671fe4ddb933c8a5f1f
webforest <raifurrahimcse13@gmail.com>
2019-06-20 10:18:38 +0600

    profile screen demo for project purposes

---
+++ app/src/main/java/com/lazycoder/profilescreendesgin/MainActivity.java
@@ -0,0 +1,30 @@
+package com.lazycoder.profilescreendesgin;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+//
+//        //17.04.19
+//        MultiDex.install(this);
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+    }
+}
